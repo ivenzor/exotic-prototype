ff6082b88d5699b0638b46bfe44f8872743ab1b8
Bergen <bergen@mooreboeck.com>
2022-09-29 01:31:41 -0700

    fix_planetary_params

--- exotic/api/colab.py
+++ exotic/api/colab.py
@@ -360,3 +360,38 @@ def make_inits_file(planetary_params, image_dir, output_dir, first_image, targ_c
   #print("Your coordinates have been saved in the initialization file here: " + inits_file_path)
   
   return(inits_file_path)
+
+  
+##############################################################
+
+def fix_planetary_params (p_param_dict):
+  for param in p_param_dict.keys():
+    if param == "Target Star RA" or param == "Target Star Dec" or param == "Planet Name" or param == "Host Star Name" or param == "Argument of Periastron (deg)":
+      continue
+    val = p_param_dict[param]
+    if val == 0.0 or np.isnan(float(val)):
+      if param == "Orbital Eccentricity (0 if null)":
+        continue
+      if param == "Ratio of Planet to Stellar Radius (Rp/Rs)":
+        p_param_dict[param] = 0.151
+      if param == "Ratio of Planet to Stellar Radius (Rp/Rs) Uncertainty":
+        p_param_dict[param] = 0.151
+        if p_param_dict["Host Star Name"] == "Qatar-6":
+          p_param_dict[param] = 0.01
+      print(f"\nIn the planetary parameters from the NASA Exoplanet Archive, \n\"{param}\" is listed as {val}.\n\n**** This might make EXOTIC crash. ****\n\nIf the parameter is *not* changed below, please edit it\nin the inits file before running EXOTIC.\n")
+  p_param_string = json.dumps(p_param_dict)
+
+  planetary_params = "\"planetary_parameters\": {\n"
+  num_done, num_total = 0, len(p_param_dict.keys())
+  for key, value in p_param_dict.items():
+    num_done += 1
+    if key == "Target Star RA" or key == "Target Star Dec" or key == "Planet Name" or key == "Host Star Name":
+      planetary_params = planetary_params + str(f"    \"{key}\": \"{value}\",\n")
+    else:
+      if num_done < num_total:
+        planetary_params = planetary_params + str(f"    \"{key}\": {value},\n")
+      else:
+        planetary_params = planetary_params + str(f"    \"{key}\": {value}\n")
+  planetary_params = planetary_params + "}"
+
+  return(planetary_params)
